bd940651d6bedcb3c8b48ba38d0d23f77ed8d257
Charles Pick <charles.pick@gmail.com>
2014-06-04 20:20:29 +0100

    more structure

--- test/test.js
+++ test/test.js
@@ -1,16 +1,28 @@
-var Modely = require('../modely');
+var Modeling = require('../lib');
 
-describe('Modely', function () {
+describe('Modeling', function () {
 
+  var Thing = Modeling({
+    '@id': {
+      label: 'ID',
+      description: 'The unique ID of the thing.',
+      type: Number,
+      default: null,
+      rules: [{name: "number", allowEmpty: true}]
+    },
+    noType: {
+      default: null
+    }
+  });
 
-  var User = Modely({
+  var User = Modeling({
     name: {
       label: 'Name',
       description: 'The name of the user.',
       type: 'string',
       rules: [
         ["required"],
-        ["pattern", /^([A-Za-z][A-Za-z0-9-]*)$/],
+        ["regexp", {pattern: /^([A-Za-z][A-Za-z0-9-]*)$/}],
       ]
     },
     email: {
@@ -18,18 +30,90 @@ describe('Modely', function () {
       description: 'The user\'s email address.',
       type: 'string',
       rules: [
-
+        {name: 'email'}
       ]
     }
   });
 
-  it('should create model classes', function () {
-    var user = new User({
-      name: 'Bob',
-      email: 'bob@example.com'
+  User.inherits(Thing);
+
+  describe('cast()', function () {
+    var user = User.cast({
+      name: 123,
+      email: true
+    });
+    it('should cast objects to the right class', function () {
+      user.should.be.instanceOf(User);
+    });
+    it('should cast properties to the right type', function () {
+      user.name.should.equal('123');
+      user.email.should.equal('true');
+    });
+  });
+
+  describe('inherits()', function () {
+    beforeEach(function () {
+      this.user = new User({
+        '@id': 'not a number',
+        name: 'admin',
+        email: true
+      });
+    });
+    it('should inherit the parent class corretly', function () {
+      this.user.should.be.instanceOf(User);
+      this.user.should.be.instanceOf(Thing);
+    });
+    it('should inherit descriptors properly', function () {
+      var result = User.validate(this.user);
+      result.valid.should.be.false;
+      result.errors.should.eql({
+        '@id': 'Expected a number.',
+        email: 'Not a valid email address.'
+      });
+    });
+  });
+
+  describe('integration', function () {
+    beforeEach(function () {
+      this.user = new User({
+        name: 123,
+        email: true
+      });
+    });
+    it('should cast property values', function () {
+      this.user.name.should.equal('123');
+      this.user.email.should.equal('true');
     });
 
-    user.validate.should.be.type('function');
-    console.log(user.validate());
+    it('should have only the defined keys', function () {
+      Object.keys(this.user).should.eql(['@id', 'noType', 'name', 'email']);
+    });
+
+    it('should validate invalid objects', function () {
+      this.user.name = 'admin';
+      this.user.email = 'nope';
+      var result = User.validate(this.user);
+      result.valid.should.be.false;
+      result.errors.should.eql({
+        email: 'Not a valid email address.'
+      });
+    });
+
+    it('should validate invalid objects with multiple errors', function () {
+      var result = User.validate(this.user);
+      result.valid.should.be.false;
+      result.errors.should.eql({
+        name: 'Does not match the required pattern.',
+        email: 'Not a valid email address.'
+      });
+    });
+
+    it('should validate valid objects', function () {
+      this.user.name = 'admin';
+      this.user.email = 'test@example.com';
+      result = User.validate(this.user);
+      result.valid.should.be.true;
+      result.errors.should.eql({});
+    });
   });
 });
